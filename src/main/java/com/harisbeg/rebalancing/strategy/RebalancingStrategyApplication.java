e2bc888d607b141e380586aff0c5a5eac3899e57
Haris Beg <haris.beg@gmail.com>
2017-03-23 13:10:43 -0700

    Starter code created by STS

---
+++ src/main/java/com/harisbeg/rebalancing/strategy/RebalancingStrategyApplication.java
@@ -0,0 +1,12 @@
+package com.harisbeg.rebalancing.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebalancingStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebalancingStrategyApplication.class, args);
+	}
+}
