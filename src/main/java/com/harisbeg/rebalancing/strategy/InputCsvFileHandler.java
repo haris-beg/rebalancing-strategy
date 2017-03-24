91b4d4f82ccde56de75a05864d7eb96009062f6e
Haris Beg <haris.beg@gmail.com>
2017-03-24 15:43:00 -0700

    Parsing CSV file withFirstRecordAsHeader - working

--- src/main/java/com/harisbeg/rebalancing/strategy/InputCsvFileHandler.java
+++ src/main/java/com/harisbeg/rebalancing/strategy/InputCsvFileHandler.java
@@ -15,16 +15,6 @@ import org.apache.commons.logging.LogFactory;
 
 public class InputCsvFileHandler implements InputFileHandler {
 	
-	public enum YahooCsvHeaders {
-		priceDate, 
-		openingPrice, 
-		highPrice,
-		lowPrice,
-		closingPrice,
-		periodVolume,
-		adjClose
-	}
-
 	private static final Log log = LogFactory.getLog(InputCsvFileHandler.class);
 	private static final String FILEPATH = "C:\\git-repos\\download-stock-histories\\csv1\\";
 	
@@ -37,13 +27,10 @@ public class InputCsvFileHandler implements InputFileHandler {
 		try {
 			log.info("Reading file " + filename);
 			Reader in = new FileReader(filename);
-			Iterable<CSVRecord> records = CSVFormat.RFC4180.withHeader(YahooCsvHeaders.class).parse(in);
+			Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
 			for (CSVRecord record : records) {
-				if (++recordCount == 1) {
-					continue;//skip the header row
-				};
 				YahooHistory yahooHistory = parseYahooHistoryRecord(record);
-				if (recordCount < 3) {
+				if (++recordCount < 3) {
 					yahooHistory.print();
 				}
 				
@@ -64,13 +51,13 @@ public class InputCsvFileHandler implements InputFileHandler {
 		YahooHistory yahooHistory = new YahooHistory();
 		try {
 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-			yahooHistory.setPriceDate(df.parse(record.get(YahooCsvHeaders.priceDate)));
-			yahooHistory.setOpeningPrice(Float.parseFloat(record.get(YahooCsvHeaders.openingPrice)));
-			yahooHistory.setHighPrice(Float.parseFloat(record.get(YahooCsvHeaders.highPrice)));
-			yahooHistory.setLowPrice(Float.parseFloat(record.get(YahooCsvHeaders.lowPrice)));
-			yahooHistory.setClosingPrice(Float.parseFloat(record.get(YahooCsvHeaders.closingPrice)));
-			yahooHistory.setAdjClose(Float.parseFloat(record.get(YahooCsvHeaders.adjClose)));
-			yahooHistory.setPeriodVolume(Long.parseLong(record.get(YahooCsvHeaders.periodVolume)));
+			yahooHistory.setPriceDate(df.parse(record.get("Date")));
+			yahooHistory.setOpeningPrice(Float.parseFloat(record.get("Open")));
+			yahooHistory.setHighPrice(Float.parseFloat(record.get("High")));
+			yahooHistory.setLowPrice(Float.parseFloat(record.get("Low")));
+			yahooHistory.setClosingPrice(Float.parseFloat(record.get("Close")));
+			yahooHistory.setAdjClose(Float.parseFloat(record.get("Adj Close")));
+			yahooHistory.setPeriodVolume(Long.parseLong(record.get("Volume")));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
