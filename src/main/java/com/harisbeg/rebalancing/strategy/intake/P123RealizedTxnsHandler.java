99202a98e77ff3d379c2a9330fa81974bd82700d
Haris Beg <haris.beg@gmail.com>
2017-03-30 18:31:58 -0700

    Buying at closing price of same date - working better

--- src/main/java/com/harisbeg/rebalancing/strategy/intake/P123RealizedTxnsHandler.java
+++ src/main/java/com/harisbeg/rebalancing/strategy/intake/P123RealizedTxnsHandler.java
@@ -14,6 +14,7 @@ import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.harisbeg.rebalancing.strategy.AppConstants;
 import com.harisbeg.rebalancing.strategy.model.P123PortfolioHistory;
@@ -22,6 +23,10 @@ import com.harisbeg.rebalancing.strategy.persistence.DbHandler;
 import com.harisbeg.rebalancing.strategy.service.DownloadSvcI;
 
 public class P123RealizedTxnsHandler implements P123RealizedTxnsHandlerI {
+	
+	@Value("${max.sell3.txns}")
+	private int maxSell3Txns;
+	
 
 	@Autowired
 	DbHandler dbHandler;
@@ -57,23 +62,36 @@ public class P123RealizedTxnsHandler implements P123RealizedTxnsHandlerI {
 					Date oldBuyDate = p123RealizedTxn.getPositionOpenDate();
 					downloadSvc.download(ticker);
 					float oldBuyPrice = dbHandler.getOpenPrice(ticker, oldBuyDate);
+					log.debug("oldBuyPrice for " + ticker + " = " + oldBuyPrice);
 					if (oldBuyPrice == -1) {
 						log.debug("Skipping " + ticker + " because of bad data");
 						continue;
 					}
 					sell3TxnCount++;
-					totalDaysHeldOld += p123RealizedTxn.getDaysHeld();
+					if (sell3TxnCount > maxSell3Txns) {
+						break;
+					}
+					int daysHeldOld = p123RealizedTxn.getDaysHeld();
+					totalDaysHeldOld += daysHeldOld;
 					Date newBuyDate = getNewBuyDate(oldBuyDate);
-					int daysHeldNew = (int) ((newBuyDate.getTime() - oldBuyDate.getTime())/86400000);
+					int daysHeldNew = daysHeldOld - (int) ((newBuyDate.getTime() - oldBuyDate.getTime())/86400000);
+					log.debug("daysHeldNew for " + ticker + " = " + daysHeldNew);
 					totalDaysHeldNew += daysHeldNew;
 					float newBuyPrice = getNewBuyPrice(ticker, newBuyDate);
+					log.debug("newBuyPrice for " + ticker + " = " + newBuyPrice);
 					double slippage = (oldBuyPrice - newBuyPrice)/oldBuyPrice;
+					log.debug("slippage for " + ticker + " = " + slippage);
 					totalSlippage += slippage;
+					if (sell3TxnCount % 50 == 0) {
+						averageSlippage = totalSlippage/sell3TxnCount;
+						log.info("Average Slippage for the first " + sell3TxnCount + " transactions = " + averageSlippage*100 + "%");
+					}
 				}
 			}
 			if (sell3TxnCount > 0) {
+				log.info("totalSlippage = " + totalSlippage);
 				averageSlippage = totalSlippage/sell3TxnCount;
-				log.info("averageSlippage = " + averageSlippage*100 + "%");
+				log.info("averageSlippage = (-ve is bad, +ve is good) " + averageSlippage*100 + "%");
 				avgDaysHeldOld = totalDaysHeldOld/sell3TxnCount;
 				log.info("avgDaysHeldOld = " + avgDaysHeldOld);
 				avgDaysHeldNew = totalDaysHeldNew/sell3TxnCount;
