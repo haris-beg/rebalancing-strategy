8c6e9aacf94b88f43ecb8ef878cebc6b77840b89
Haris Beg <haris.beg@gmail.com>
2017-04-03 21:08:48 -0700

    Externalizing Business Rules in Properties files

--- src/main/java/com/harisbeg/rebalancing/strategy/service/BusinessRulesSvcImpl.java
+++ src/main/java/com/harisbeg/rebalancing/strategy/service/BusinessRulesSvcImpl.java
@@ -3,22 +3,35 @@ package com.harisbeg.rebalancing.strategy.service;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.harisbeg.rebalancing.strategy.persistence.DbHandler;
-
+import com.harisbeg.rebalancing.strategy.utils.DateUtils;
 public class BusinessRulesSvcImpl implements BusinessRulesSvc {
+
+	@Value("${buying.time.of.day}")
+	private String buyingTimeOfDay;
+
+	@Value("${buy.date.offset}")
+	private int buyDateOffset;
 
 	@Autowired
 	DbHandler dbHandler;
 
 	@Override
 	public Date getNewBuyDate(Date oldBuyDate) {
-		return oldBuyDate;
+		return DateUtils.getNewDate(oldBuyDate, buyDateOffset);
 	}
 
 	@Override
 	public float getNewBuyPrice(String ticker, Date newBuyDate) {
-		return dbHandler.getClosingPrice(ticker, newBuyDate);
+		float newBuyPrice = -1;
+		if (buyingTimeOfDay.equalsIgnoreCase("close")) {
+			newBuyPrice = dbHandler.getClosingPrice(ticker, newBuyDate);
+		} else if (buyingTimeOfDay.equalsIgnoreCase("open")) {
+			newBuyPrice = dbHandler.getOpenPrice(ticker, newBuyDate);
+		}
+		return newBuyPrice;
 	}
 
 }
